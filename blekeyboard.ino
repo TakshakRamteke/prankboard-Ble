e1d6007e8a5d3fc10c2533487d69b740faf2f68c
TakshakRamteke <takshakramteke0708@gmail.com>
2021-08-23 20:28:03 +0530

    enhancement:added Comments
    added comments to make the code easy to understand if someone wants to make changes to it and come up with a better prank

--- blekeyboard.ino
+++ blekeyboard.ino
@@ -1,29 +1,29 @@
+/* An Implementation of the Bluetooth keyboard librerary avaliable for ESP32 just 
+to mess with my friend and anoy them and have a little fun */
+
 #include <BleKeyboard.h>
 
 BleKeyboard bleKeyboard;
 
 void setup() {
-  Serial.begin(115200);
-  Serial.println("Starting BLE work!");
-  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(115200); // Initializing the Serial Monitor
   bleKeyboard.begin();
+  Serial.println("Started the Bluetooth Please connect ...");
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop() {
   if(bleKeyboard.isConnected()) {
-    digitalWrite(LED_BUILTIN, HIGH);
-    Serial.println("Sending 'Hello world'...");
-    bleKeyboard.print("Hello world");
-    Serial.println("Sending Enter key...");
-    bleKeyboard.write(KEY_RETURN);
-    bleKeyboard.press(KEY_LEFT_CTRL);
-    bleKeyboard.press(KEY_LEFT_ALT);
-    bleKeyboard.press(KEY_DELETE);
-    delay(500);
+    Serial.println("Bluettoth connected !!!");
+    Serial.println("Starting the procedure");
+    digitalWrite(LED_BUILTIN, HIGH); // blinking the LED in case the Serial monitor isnt accessible
+    bleKeyboard.press(KEY_LEFT_CTRL); // Pressing the left control key
+    bleKeyboard.press(KEY_LEFT_ALT); // Pressing the left alter key
+    bleKeyboard.press(KEY_DELETE); // Pressing the Delete key
+    delay(500); // adding a delay of 5 sec to ensure the prank works :devil_smiley:
+    bleKeyboard.releaseAll(); // releasing the key
   }
-  bleKeyboard.releaseAll();
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(500);
+  digitalWrite(LED_BUILTIN, LOW); // Turning the built in LED off to let us know that the prank was successfull
   Serial.println("Waiting 5 seconds...");
-  delay(500);
+  delay(500); // adding a delay of 5 sec before the prank restarts :intense_laughs:
 }
